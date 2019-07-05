d1aed65779d8bdd79678a72824ee061a69ec8302
EdZa <edalzap@gmail.com>
2019-07-05 10:06:24 -0500

    Improved script to add sites in local.conf.

--- addLocal.bash
+++ addLocal.bash
@@ -1,10 +1,16 @@
 #!/bin/bash
-read -p "Blocked URL:" u
+read -p "Domain to Add: " u
 U=$(echo ${u} | sed 's|http://||g; s|https://||g')
-echo "address=/${U}/127.0.0.1" >> local.conf
+
+read -p "IP to redirect ${U} [Use 127 to block domain]: " ip
+if [ ${ip} == "127" ]; then
+ echo "address=/${U}/127.0.0.1" >> local.conf
+else
+ echo "address=/${U}/${ip}" >> local.conf
+fi
 
 #  clean duplicate sites
 sort local.conf | uniq > /tmp/.blocked.tmp
 mv /tmp/.blocked.tmp local.conf
-echo "=> Total Blocked Domains: <="
+echo "=> Total Local Domains: <="
 wc -l local.conf
