309a3df8fdf2a0d4301cb9d3ebfec330007e97c4
EdZa <edalzap@gmail.com>
2019-07-04 22:39:55 -0500

    Added blocked sites. Fixed bug in script to add sites.

--- addBlock.bash
+++ addBlock.bash
@@ -1,4 +1,10 @@
 #!/bin/bash
 read -p "Blocked URL:" u
 U=$(echo ${u} | sed 's|http://||g; s|https://||g')
-echo ${U} >> ads.conf
+echo "address=/${U}/127.0.0.1" >> ads.conf
+
+#  clean duplicate sites
+sort ads.conf | uniq > /tmp/.blocked.tmp
+mv /tmp/.blocked.tmp ads.conf
+echo "=> Total Blocked Domains: <="
+wc -l ads.conf
